a94826fe95cb00c3e9879e51390bbfd0dfcdd4a1
Prakarsh-Kaushik <prakarsh.kr@gmail.com>
2021-09-30 18:06:03 +0530

    Project 6

---
+++ Project 6 (Heads or Tails)/main.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+randomNum = random.randint(0,1)
+
+if(randomNum == 1):
+  print("Heads")
+elif (randomNum==0):
+  print("Tails")
+
+
+
+
+
+
+
+
